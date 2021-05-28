054195612fcb943e9b0970e4058bdf671d2eb89b
Ericsson0 <80060414+Ericsson0@users.noreply.github.com>
2021-05-28 12:07:33 -0400

    Update __init__.py

--- Clinic1.1.1 copy/flaskblog/__init__.py
+++ Clinic1.1.1 copy/flaskblog/__init__.py
@@ -30,8 +30,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True 
-app.config['MAIL_USERNAME'] = 'clinicdonotreply@gmail.com'
-app.config['MAIL_PASSWORD'] = 'idontcarelol'
+app.config['MAIL_USERNAME'] = 'your email'
+app.config['MAIL_PASSWORD'] = 'your email password'
 mail = Mail(app) 
 
 from flaskblog import routes
